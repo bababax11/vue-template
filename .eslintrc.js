64c6633d71f1fa69bdad639ff9c3be82503508c9
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-03-10 04:17:22 +0900

    lint: trailing comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
     '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -23,8 +23,8 @@ module.exports = {
       {
         singleQuote: true,
         semi: false,
-        'arrow-palens': true
-      }
+        trailingComma: 'es5',
+      },
     ],
     'arrow-body-style': 'error',
     // 'arrow-parens': 'error',
@@ -51,24 +51,24 @@ module.exports = {
         format: ['PascalCase'],
         custom: {
           regex: 'send|start|find',
-          match: false
-        }
+          match: false,
+        },
       },
       {
         selector: 'parameter',
-        format: ['camelCase']
-      }
-    ]
+        format: ['camelCase'],
+      },
+    ],
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 }
